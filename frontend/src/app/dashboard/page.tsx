96ca3de69a2c6356c9b2dea42141979f80ccd382
PrathamDwivedi27 <prathamd549@gmail.com>
2024-11-09 12:34:04 +0530

    [Feat]: Logout Feautre completed

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
+import DashNav from '@/components/dashboard/DashNav'
 import React from 'react'
+import { authOptions, CustomSession } from '../api/auth/[...nextauth]/options'
+import { getServerSession } from 'next-auth'
 
-const dashboard = () => {
+const dashboard = async () => {
+    const session:CustomSession |null=await getServerSession(authOptions);
+
   return (
     <div>
-      
+      <DashNav name={session?.user?.name ?? ''} image={session?.user?.image ?? ''}/>
     </div>
   )
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import SessionProvider from "@/providers/SessionProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
+        <Toaster richColors duration={4000}/>
         {children}
       </body>
       </SessionProvider>
